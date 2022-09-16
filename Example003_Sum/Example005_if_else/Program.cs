4ccb6ec12bffc50b6c53942bdcefb423ff2d9c85
Roman <roman7822@mail.ru>
2022-09-16 12:29:47 +0300

    Создали программу приветствия особенного человека

---
+++ Example003_Sum/Example005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:  ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маргарита")
+{
+    Console.WriteLine("Привет, Любимая");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
